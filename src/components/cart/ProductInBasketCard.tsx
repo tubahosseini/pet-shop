53625f7aa78ddaea9493f378561c0b53277c0b6d
Tuba Hosseini <tubahosseini@gmail.com>
2024-08-08 04:49:20 +0330

    feat(cart): show the selected products in basket

--- src/components/cart/ProductInBasketCard.tsx
+++ src/components/cart/ProductInBasketCard.tsx
@@ -1,24 +1,29 @@
 import { Box, Card, Typography } from "@mui/material";
 import Image from "next/image";
 import React from "react";
-import test1 from "@/assets/images/home/test1.jpg";
 
-export default function ProductInBasketCard() {
+export default function ProductInBasketCard({ product }: { product: any }) {
   return (
     <Card
       sx={{
         p: 1,
         display: "flex",
         cursor: "pointer",
-        "&:hover": { bgcolor: "pink" },
+        "&:hover": { bgcolor: "lightgray" },
+        mb:1
       }}
     >
-      <Image src={test1} alt="test" width={90} />
+      <Image
+        src={`http://${product.images}`}
+        alt="test"
+        width={90}
+        height={90}
+      />
       <Box sx={{ pl: 1 }}>
-        <Typography>
-          Scrumbles Complete Adult & Senior Dry Cat Food Chicken & Salmon
-        </Typography>
-        <Box>$ 34</Box>
+        <Typography>{product.name}</Typography>
+        <Box>
+          € {product.price} x {product.quantityInBasket} = € {product.price * product.quantityInBasket}
+        </Box>
       </Box>
     </Card>
   );
--- src/layout/header/CartDrawer.tsx
+++ src/layout/header/CartDrawer.tsx
@@ -1,12 +1,18 @@
 import ProductInBasketCard from "@/components/cart/ProductInBasketCard";
+import { routes } from "@/constants/routes";
+import { useProductStore } from "@/stores/BasketStore";
 import { Box, Button, Divider, Typography } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 export default function CartDrawer() {
+  const cart = useProductStore((state) => state.cart);
+  // console.log(cart);
+
   return (
     <Box
       sx={{
-        width: 300,
+        width: 350,
         mx: 1,
         display: "flex",
         flexDirection: "column",
@@ -17,22 +23,33 @@ export default function CartDrawer() {
       <Box>
         <Typography sx={{ fontSize: 20, my: 1 }}>Basket</Typography>
         <Divider sx={{ mb: 2 }} />
-        <ProductInBasketCard />
+        {cart?.map((product) => (
+          <ProductInBasketCard key={product._id} product={product} />
+        ))}
       </Box>
       <Box sx={{ mb: 2 }}>
         <Divider />
-        <Typography sx={{ fontSize: 20, my: 1 }}>Total price: $ 100</Typography>
-        <Button
-          sx={{
-            width: "100%",
-            bgcolor: "primary.main",
-            color: "primary.light",
-            "&:hover": { bgcolor: "primary.main", color: "primary.light" },
-            mt: 1,
-          }}
-        >
-          checkout
-        </Button>
+        <Typography sx={{ fontSize: 20, my: 1 }}>
+          Total price: €{" "}
+          {cart.reduce(
+            (total, product) =>
+              total + product.price * product.quantityInBasket,
+            0
+          )}
+        </Typography>
+        <Link href={routes.checkout}>
+          <Button
+            sx={{
+              width: "100%",
+              bgcolor: "primary.main",
+              color: "primary.light",
+              "&:hover": { bgcolor: "primary.main", color: "primary.light" },
+              mt: 1,
+            }}
+          >
+            checkout
+          </Button>
+        </Link>
       </Box>
     </Box>
   );
